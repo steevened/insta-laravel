61d097e0b6244107490a00d18f2040122aea4963
steven <alvaradosteven315@gmail.com>
2023-06-18 00:00:34 -0500

    install tailwindcss for views

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Devstagram - @yield('title')</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite('resources/css/app.css')
+      
+    </head>
+    <body class="bg-gray-900 text-gray-200 px-5 py-10">
+      
+    </body>
+</html>
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('title')
+    Shop
+@endsection
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-      
-    </head>
-    <body>
-        <nav>
-            <a href="/">Home</a>
-            <a href="/team">Team</a>
-            <a href="/shop">Shop</a>
-        </nav>
-        <h1>Shop</h1>
-    </body>
-</html>
+@section('content')
+    Items for you
+@endsection
